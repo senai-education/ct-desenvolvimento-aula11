e42504617ce5fc57b7f4444d966609b2626d1796
Gustavo Souza <gustavorobertotb@outlook.com>
2020-12-10 22:26:13 -0300

    :construction: Feat: Ajusted code

--- Validator/src/validator/Validator.java
+++ Validator/src/validator/Validator.java
@@ -3,30 +3,46 @@ package validator;
 public class Validator {
 
     public static void main(String[] args) {
-        String t = "09937032911";
-        int resultadoSomaLoca = 0;
-        int indiceContrario = 10;
+          String cpf = "50119166003";
+        int primeiroDigitoVerificador = Integer.parseInt(String.valueOf(cpf.charAt(9)));
+        int seqValidaPrimeiroDigito = 10;
+        int somaValidaPrimeiroDigito = 0;
+        int restoDivisaoPrimeiroDigito;
         for(int i = 0; i < 9; i++){
-           resultadoSomaLoca += Integer.parseInt(String.valueOf(t.charAt(i))) * indiceContrario;
-           indiceContrario--;
-        }             
-            
-        int validacaoUm = (resultadoSomaLoca * 10) % t.length();
-        if(validacaoUm == Integer.parseInt(String.valueOf(t.charAt(9)))){
-            System.out.println("VALIDOU PRIMEIRO DIGITO");
+            somaValidaPrimeiroDigito += (Integer.parseInt(String.valueOf(cpf.charAt(i))) * seqValidaPrimeiroDigito);
+            seqValidaPrimeiroDigito--;
+        }
+        restoDivisaoPrimeiroDigito = (somaValidaPrimeiroDigito * 10) % cpf.length();
+        if(restoDivisaoPrimeiroDigito == 10){
+            restoDivisaoPrimeiroDigito = 0;
+        }
+        System.out.println(restoDivisaoPrimeiroDigito);
+        if(restoDivisaoPrimeiroDigito == primeiroDigitoVerificador){
+            System.out.println("PRIMEIRO DIGITO FOI VALIDADO");
+        }else {
+            System.out.println("PRIMEIRO DIGITO NÃO É VALIDO E CONSEQUENTEMENTE CPF TAMBÉM É");
         }
         
-        int resultadoSomaLoca2 = 0;
-        int indiceContrario2 = 11;
+        /*----------------------------------*/
+        int segundoDigitoVerificador = Integer.parseInt(String.valueOf(cpf.charAt(10)));
+        int seqValidaSegundoDigito = 11;
+        int somaValidaSegundoDigito = 0;
+        int restoDivisaoSegundoDigito;
         for(int i = 0; i < 10; i++){
-           resultadoSomaLoca2 += Integer.parseInt(String.valueOf(t.charAt(i))) * indiceContrario2;
-           indiceContrario2--;
-        }             
-            
-        int validacaoDois = (resultadoSomaLoca2 * 10) % t.length();
-        if(validacaoDois == Integer.parseInt(String.valueOf(t.charAt(10)))){
-            System.out.println("VALIDOU SEGUNDO DIGITO");
+            somaValidaSegundoDigito += (Integer.parseInt(String.valueOf(cpf.charAt(i))) * seqValidaSegundoDigito);
+            seqValidaSegundoDigito--;
         }
+        restoDivisaoSegundoDigito = (somaValidaSegundoDigito * 10) % cpf.length();
+        if(restoDivisaoSegundoDigito == 10){
+            restoDivisaoSegundoDigito = 0;
+        }
+        System.out.println(restoDivisaoSegundoDigito);
+        if(restoDivisaoSegundoDigito == segundoDigitoVerificador){
+            System.out.println("SEGUNDO DIGITO FOI VALIDADO");
+        }else {
+            System.out.println("SEGUNDO DIGITO NÃO É VALIDO E CONSEQUENTEMENTE CPF TAMBÉM É");
+        }
+        
     }
     
 }
